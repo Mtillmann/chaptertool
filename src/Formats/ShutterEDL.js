4c692779c76376a1d05610bc3c994c17b84a6b38
Martin Tillmann <mtillmann@gmail.com>
2024-01-30 10:00:44 +0100

    Fix ShutterEDL format issues and add new format support

--- src/Formats/ShutterEDL.js
+++ src/Formats/ShutterEDL.js
@@ -3,18 +3,26 @@ import { FormatBase } from "./FormatBase.js";
 
 export class ShutterEDL extends FormatBase {
 
+    // this format is based on the shutter encoder edl format
+    // https://github.com/paulpacifico/shutter-encoder/blob/f3d6bb6dfcd629861a0b0a50113bf4b062e1ba17/src/application/SceneDetection.java
+
     detect(inputString) {
         return /^TITLE:\s.*\r?\n/.test(inputString.trim());
     }
 
     decodeTime(timeString) {
-        return timeString.replace(/:(\d+)$/,'.$1');
+        return timeString.replace(/:(\d+)$/,'.$10');
     }
 
     encodeTime(time) {
+        // since this format apparently expects the end time of the next item and the previous start time
+        // to be the same, 
+        // I'll round them to look like they looked in my sample file when converting
+        // from shutter edl to shutter edl...
+
         const string = secondsToTimestamp(time, {milliseconds: true});
-        //todo handle ms better here... 
-        return string.replace(/\.(\d+)$/,':$1');
+        const ms = String(Math.ceil(parseInt(string.split('.').pop()) * 0.1));
+        return string.replace(/\.(\d+)$/,`:${ms.padStart(2, '0')}`);
     }
 
     parse(input) {
@@ -34,6 +42,8 @@ export class ShutterEDL extends FormatBase {
                 if (acc.at(-1)?.startTime === startTime) {
                     return acc;
                 }
+
+                console.log(startTime, endTime, title);
                 
                 acc.push({
                     startTime,
@@ -44,24 +54,9 @@ export class ShutterEDL extends FormatBase {
             }, []);
     }
 
-    /*
-    TITLE: bunny-dings
-000001  BigBuckBunny_320x180.mp4 V     C        00:00:00:00 00:00:11:21 00:00:00:00 00:00:11:21
-000002  BigBuckBunny_320x180.mp4 A     C        00:00:00:00 00:00:11:21 00:00:00:00 00:00:11:21
-000003  BigBuckBunny_320x180.mp4 A2    C        00:00:00:00 00:00:11:21 00:00:00:00 00:00:11:21
-000004  BigBuckBunny_320x180_cut.mp4 V     C        00:00:11:21 00:00:15:18 00:00:11:21 00:00:15:18
-000005  BigBuckBunny_320x180_cut.mp4 A     C        00:00:11:21 00:00:15:18 00:00:11:21 00:00:15:18
-000006  BigBuckBunny_320x180_cut.mp4 A2    C        00:00:11:21 00:00:15:18 00:00:11:21 00:00:15:18
-000007  BigBuckBunny_320x180.mp4 V     C        00:00:15:18 00:00:23:01 00:00:15:18 00:00:23:01
-000008  BigBuckBunny_320x180.mp4 A     C        00:00:15:18 00:00:23:01 00:00:15:18 00:00:23:01
-000009  BigBuckBunny_320x180.mp4 A2    C        00:00:15:18 00:00:23:01 00:00:15:18 00:00:23:01
-000010  BigBuckBunny_320x180_cut.mp4 V     C        00:00:23:01 00:00:47:17 00:00:23:01 00:00:47:17
-000011  BigBuckBunny_320x180_cut.mp4 A     C        00:00:23:01 00:00:47:17 00:00:23:01 00:00:47:17
-000012  BigBuckBunny_320x180_cut.mp4 A2    C        00:00:23:01 00:00:47:17 00:00:23:01 00:00:47:17
-000013  BigBuckBunny_320x180.mp4 V     C        00:00:47:17 00:00:56:02 00:00:47:17 00:00:56:02
-*/
-
     toString() {
+        // this format is weird, it expects 3 tracks per chapter, i suspect it's
+        // V = video, A, A2 = stereo audio
         const tracks = ['V', 'A', 'A2'];
         const output = this.chapters.reduce((acc, chapter,i) => {
             
@@ -75,7 +70,6 @@ export class ShutterEDL extends FormatBase {
             return acc;
         }, []);
         
-
         output.unshift('TITLE: ' + this.meta.title);
         return output.join("\n");
     }
