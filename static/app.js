cdd3e9cb97aca09a0e061f21960d990e9531a539
Martin Tillmann <mtillmann@gmail.com>
2025-03-18 20:54:46 +0100

    Refactor settings dialog to use available formats and update selection handling

--- static/app.js
+++ static/app.js
@@ -26408,43 +26408,60 @@ Z`;
 
   var SettingsDialog = {
 
+    availableFormats: [],
     selectedFormats: [],
     settingsModal: null,
     initSettingsDialog () {
-      const storedFormats = JSON.parse(localStorage.getItem('ct-selectedFormats') ?? '[]');
+      this.selectedFormats = JSON.parse(localStorage.getItem('ct-selectedFormats') ?? '[]');
+      if (this.selectedFormats.length === 0) {
+        this.selectedFormats = Object.keys(AutoFormat.classMap).filter(key => key !== 'ffmpeginfo');
+      }
 
-      this.selectedFormats = Object.entries(AutoFormat.classMap).reduce((acc, entry) => {
+      this.availableFormats = Object.entries(AutoFormat.classMap).reduce((acc, entry) => {
         const [key, value] = entry;
         if (key !== 'ffmpeginfo') {
           acc.push({
             key,
-            name: value.name,
-            selected: storedFormats.length === 0 || storedFormats.includes(key)
+            name: value.name
           });
         }
         return acc
       }, []).sort((a, b) => a.name.localeCompare(b.name));
 
       this.settingsModal = new Modal(this.$refs.settingsDialog);
-
-      this.settingsModal.show();
     },
 
-    storeSettings () {
-      const keys = this.selectedFormats.reduce((acc, item) => {
-        if (item.selected) {
-          acc.push(item.key);
-        }
-        return acc
+    toggleFormat (key) {
+      if (this.selectedFormats.includes(key)) {
+        this.selectedFormats = this.selectedFormats.filter(item => item !== key);
+      } else {
+        this.selectedFormats.push(key);
       }
-      , []);
 
-      localStorage.setItem('ct-selectedFormats', JSON.stringify(keys));
+      localStorage.setItem('ct-selectedFormats', JSON.stringify(this.selectedFormats));
     },
 
-    showSettingsDialog (state) {
+    showSettingsDialog () {
       this.settingsModal.show();
+    },
+    isFormatSelectedClass (key, key2 = null) {
+      const keySelected = !(this.availableFormats.find(item => item.key === key).selected);
+      const key2Selected = !(key2 ? this.availableFormats.find(item => item.key === key2).selected : true);
+
+      console.log({
+        key,
+        key2,
+        keySelected,
+        key2Selected,
+        x: !keySelected && !key2Selected
+      });
+
+      // return an object that contains d-none when both keySelected and key2Selected are false
+      return { 'd-none': !keySelected && !key2Selected }
+
+      // return { xxx:true, 'd-none': !keySelected && !key2Selected }
     }
+
   };
 
   window.Alpine = module_default;
